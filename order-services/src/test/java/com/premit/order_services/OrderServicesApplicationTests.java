5f0de71c0f035337603d48f7b0a7b578534b7822
premsai7744 <premsai7744@gmail.com>
2026-06-24 23:19:47 +0530

    Initial commit

---
+++ order-services/src/test/java/com/premit/order_services/OrderServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premit.order_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
